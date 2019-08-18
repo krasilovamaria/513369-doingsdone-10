3cd7e7024d8f9604568dcb2aa5800ec3801f173e
maria <marivasi11@yandex.ru>
2019-08-18 11:03:15 +0700

    Исправляет замечания при написании SQL

--- schema.sql
+++ schema.sql
@@ -2,29 +2,29 @@ CREATE DATABASE doingsdone CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE doingsdone;
 
 CREATE TABLE user (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  date datetime NOT NULL COMMENT 'DEAFAULT NOW()',
-  email char(128) NOT NULL UNIQUE,
-  name char(128) NOT NULL,
+  id       int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date     datetime NOT NULL COMMENT 'DEAFAULT NOW()',
+  email    char(128) NOT NULL UNIQUE,
+  name     char(128) NOT NULL,
   password varchar(128) NOT NULL
 );
 
 CREATE TABLE project (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name char(128) NOT NULL,
+  id        int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name      char(128) NOT NULL,
   author_id int(11) NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user(id)
 );
 
 CREATE TABLE task (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  date datetime NOT NULL COMMENT 'DEFAULT NOW()',
-  status tinyint(1) DEFAULT NULL,
-  name char(128) NOT NULL,
-  file char(128) DEFAULT NULL,
-  deadline datetime DEFAULT NULL,
-  author_id INTEGER NOT NULL,
-  project_id INTEGER NOT NULL,
+  id         int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date       datetime NOT NULL COMMENT 'DEFAULT NOW()',
+  status     tinyint(1) DEFAULT NULL,
+  name       char(128) NOT NULL,
+  file       char(128) DEFAULT NULL,
+  deadline   datetime DEFAULT NULL,
+  author_id  int(11) NOT NULL,
+  project_id int(11) NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user(id),
   FOREIGN KEY (project_id) REFERENCES project(id)
 );
