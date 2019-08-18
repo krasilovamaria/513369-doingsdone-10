04cb99b26f1efb5031abe7b712f3ea547a4ae993
maria <marivasi11@yandex.ru>
2019-08-18 10:50:31 +0700

    Исправления синтаксиса SQL

--- schema.sql
+++ schema.sql
@@ -1,29 +1,30 @@
-CREATE DATABASE doingsdone
+CREATE DATABASE doingsdone CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE doingsdone;
 
-CREATE TABLE user
-(
-  id        INTEGER NOT NULL AUTO_INCREMENT,
-  date      DATETIME NOT NULL,
-  email     CHAR(50) NOT NULL,
-  name      CHAR(50) NOT NULL,
-  password  VARCHAR(50) NOT NULL,
-)
+CREATE TABLE user (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date datetime NOT NULL COMMENT 'DEAFAULT NOW()',
+  email char(128) NOT NULL UNIQUE,
+  name char(128) NOT NULL,
+  password varchar(128) NOT NULL
+);
 
-CREATE TABLE project
-(
-  id        INTEGER NOT NULL AUTO_INCREMENT,
-  name      CHAR(50) NOT NULL,
-  author_id INTEGER NOT NULL FOREIGN KEY REFERENCES user(id),
-)
+CREATE TABLE project (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name char(128) NOT NULL,
+  author_id int(11) NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user(id)
+);
 
-CREATE TABLE task
-(
-  id          INTEGER NOT NULL AUTO_INCREMENT,
-  date        DATETIME,
-  status      INTEGER,
-  name        CHAR(50) NOT NULL,
-  file        INTEGER,
-  validity    DATETIME,
-  author_id   INTEGER NOT NULL FOREIGN KEY REFERENCES user(id),
-  project_id  INTEGER NOT NULL FOREIGN KEY REFERENCES project(id),
-)
+CREATE TABLE task (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date datetime NOT NULL COMMENT 'DEFAULT NOW()',
+  status tinyint(1) DEFAULT NULL,
+  name char(128) NOT NULL,
+  file char(128) DEFAULT NULL,
+  deadline datetime DEFAULT NULL,
+  author_id INTEGER NOT NULL,
+  project_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user(id),
+  FOREIGN KEY (project_id) REFERENCES project(id)
+);
