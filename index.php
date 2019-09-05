07ed9a0f480c3097eaaf65fd74645da5343d8ad6
maria <marivasi11@yandex.ru>
2019-09-05 10:25:02 +0700

    Выполнение задачи

--- index.php
+++ index.php
@@ -10,6 +10,35 @@ if (empty($_SESSION['user_id'])) {
     exit;
 }
 
+/* проверяет существование куки, если кука существует,
+то показывает выполненные задачи или нет*/
+$show_task = 'show_task';
+
+if (isset($_COOKIE['show_task'])) {
+    $show_complete_tasks = intval($_COOKIE['show_task']);
+}
+/* устанавливает куку*/
+setcookie("show", $show_complete_tasks);
+
+/* показывает выполненные задачи, после нажатия на чекбокс*/
+if (isset($_GET['id'])) {
+    $task_id = intval($_GET['id']);
+    $sql = "SELECT * FROM task WHERE id = '$task_id'";
+    $result = mysqli_query($connect, $sql);
+    $task_complete = mysqli_fetch_assoc($result);
+
+    /* инвертирует статус задачи выполнена -> не выполнена, не выполнена -> выполнена*/
+    if($task_complete['status'] === 0) {
+        $status = 1;
+    } else {
+        $status = 0;
+    }
+
+    /* обновляет task в бд*/
+    $sql_update = "UPDATE task SET status = '$status' WHERE id ='$task_id'";
+    $res = mysqli_query($connect, $sql_update);
+}
+
 /* получает список проектов*/
 /* если параметра нет, то NULL(показывает задачи как есть)*/
 $project_id = $_GET['project_id'] ?? null;
@@ -27,6 +56,8 @@ $tasks = getTasks($connect, $project_id);
 if (count($tasks) === 0) {
     print404Page($user_name, $projects, $show_complete_tasks);
 }
+
+
 
 /* подключение контента через include_template*/
 $page_content = include_template('main.php', [
