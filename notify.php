af3b40ea3bd265799904db7f586964132dcf5730
maria <marivasi11@yandex.ru>
2019-09-14 18:18:11 +0700

    Электронное письмо

--- notify.php
+++ notify.php
@@ -2,20 +2,29 @@
 require_once('config/init.php');
 require_once('vendor/autoload.php');
 
-/* получает список задач*/
-$tasks = getTasks($connect, $user_id, $project_id, $filter, $show_complete_tasks);
+/* получает список задач на сегодня*/
+$sql = "SELECT u.id AS id, email, u.name AS name FROM task t
+LEFT JOIN user u ON t.author_id = u.id WHERE status = 0
+AND deadline = CURDATE() GROUP BY u.id";
+$result = mysqli_query($connect, $sql);
+$users_current_date = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 /* если задачи есть отправляет письмо*/
-if (!empty($tasks)) {
+if (!empty($users_current_date)) {
     /* создает объект transport для отправки письма*/
     $transport = new Swift_SmtpTransport('phpdemo.ru', 25); /* SMTP сервер: phpdemo.ru, порт:25*/
     $transport->setUsername('keks@phpdemo.ru'); /* отправитель: keks@phpdemo.ru*/
     $transport->setPassword('htmlacademy'); /* пароль: htmlacademy*/
     $mailer = new Swift_Mailer($transport); /* объект библиотеки SwiftMailer, ответственный за отправку сообщений*/
     /* формирует само письмо*/
-    foreach ($tasks as $key => $value) {
+    foreach ($users_current_date as $key => $value) {
         $msg = "";
         $msg_content = "";
+        /* получает задачи для определенного пользователя*/
+        $sql_tasks = "SELECT * FROM task WHERE status = 0 AND deadline = CURDATE() AND author_id = " . $value['id'];
+        $result = mysqli_query($connect, $sql_tasks);
+        $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
         /* меняет имя для каждого пользователя*/
         $msg_content = "Уважаемый(-ая) " . $value['name'] . " у вас запланирована задача(-и) ";
         /* обнуляет переменную для работы*/
--- templates/add_main.php
+++ templates/add_main.php
@@ -1,48 +1,49 @@
-<h2 class="content__main-heading">Добавление задачи</h2>
+<main class="content__main">
+    <h2 class="content__main-heading">Добавление задачи</h2>
 
-<form class="form" action="add.php" method="post" autocomplete="off" enctype="multipart/form-data">
-    <div class="form__row">
-        <label class="form__label" for="name">Название <sup>*</sup></label>
+    <form class="form" action="add.php" method="post" autocomplete="off" enctype="multipart/form-data">
+        <div class="form__row">
+            <label class="form__label" for="name">Название <sup>*</sup></label>
 
-        <input class="form__input
-            <?php if (!empty($errors['name'])) : ?> form__input--error <?php endif /* добавляет класс form__input--error если поле пустое*/ ?>"
-             type="text" name="name" id="name" value="" placeholder="Введите название">
-        <?php if (!empty($errors['name'])) : ?><p class="form__message"><?= $errors['name'] ?></p><?php endif /* добавляет в контейнер поля новый тег p.form__message*/ ?>
-    </div>
-
-    <div class="form__row">
-        <label class="form__label" for="project">Проект <sup>*</sup></label>
-
-        <select class="form__input form__input--select <?php if (!empty($errors['project'])) : ?>form__input--error<?php endif ?>" name="project" id="project">
-            <?php foreach ($projects as $item) : /*добавляет список проектов из массива $projects*/ ?>
-                <option value="<?php echo $item['id'] ?>"><?= htmlspecialchars($item['name']); ?></option>
-            <?php endforeach; ?>
-        </select>
-        <?php if (!empty($errors['project'])) : ?><p class="form__message"><?= $errors['project'] ?></p><?php endif ?>
-    </div>
-
-    <div class="form__row">
-        <label class="form__label" for="date">Дата выполнения</label>
-
-        <input class="form__input form__input--date<?php if (!empty($errors['date'])) : ?>
-                form__input--error <?php endif ?>" type="text" name="date" id="date" value="<?= $_POST['date'] ?? "" ?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
-        <?php if (!empty($errors['date'])) : ?><p class="form__message"><?= $errors['date'] ?></p> <?php endif ?>
-    </div>
-
-    <div class="form__row">
-        <label class="form__label" for="file">Файл</label>
-
-        <div class="form__input-file">
-            <input class="visually-hidden" type="file" name="file" id="file" value="">
-
-            <label class="button button--transparent" for="file">
-                <span>Выберите файл</span>
-            </label>
-            <?php if (!empty($errors['file'])) : ?><p class="form__message"><?= $errors['file'] ?></p> <?php endif ?>
+            <input class="form__input
+            <?php if (!empty($errors['name'])) : ?> form__input--error <?php endif /* добавляет класс form__input--error если поле пустое*/ ?>" type="text" name="name" id="name" value="" placeholder="Введите название">
+            <?php if (!empty($errors['name'])) : ?><p class="form__message"><?= $errors['name'] ?></p><?php endif /* добавляет в контейнер поля новый тег p.form__message*/ ?>
         </div>
-    </div>
 
-    <div class="form__row form__row--controls">
-        <input class="button" type="submit" name="" value="Добавить">
-    </div>
-</form>
+        <div class="form__row">
+            <label class="form__label" for="project">Проект <sup>*</sup></label>
+
+            <select class="form__input form__input--select <?php if (!empty($errors['project'])) : ?>form__input--error<?php endif ?>" name="project" id="project">
+                <?php foreach ($projects as $item) : /*добавляет список проектов из массива $projects*/ ?>
+                    <option value="<?php echo $item['id'] ?>"><?= htmlspecialchars($item['name']); ?></option>
+                <?php endforeach; ?>
+            </select>
+            <?php if (!empty($errors['project'])) : ?><p class="form__message"><?= $errors['project'] ?></p><?php endif ?>
+        </div>
+
+        <div class="form__row">
+            <label class="form__label" for="date">Дата выполнения</label>
+
+            <input class="form__input form__input--date<?php if (!empty($errors['date'])) : ?>
+                form__input--error <?php endif ?>" type="text" name="date" id="date" value="<?= $_POST['date'] ?? "" ?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <?php if (!empty($errors['date'])) : ?><p class="form__message"><?= $errors['date'] ?></p> <?php endif ?>
+        </div>
+
+        <div class="form__row">
+            <label class="form__label" for="file">Файл</label>
+
+            <div class="form__input-file">
+                <input class="visually-hidden" type="file" name="file" id="file" value="">
+
+                <label class="button button--transparent" for="file">
+                    <span>Выберите файл</span>
+                </label>
+                <?php if (!empty($errors['file'])) : ?><p class="form__message"><?= $errors['file'] ?></p> <?php endif ?>
+            </div>
+        </div>
+
+        <div class="form__row form__row--controls">
+            <input class="button" type="submit" name="" value="Добавить">
+        </div>
+    </form>
+</main>
