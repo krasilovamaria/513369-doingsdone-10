4978d656b3b01e9e8d997cacd1f1d5dcef270037
maria <marivasi11@yandex.ru>
2019-09-14 18:46:08 +0700

    Электронное письмо

--- notify.php
+++ notify.php
@@ -3,9 +3,9 @@ require_once('config/init.php');
 require_once('vendor/autoload.php');
 
 /* получает список задач на сегодня*/
-$sql = "SELECT u.id AS id, email, u.name AS name FROM task t
+$sql = 'SELECT u.id AS id, email, u.name AS name FROM task t
 LEFT JOIN user u ON t.author_id = u.id WHERE status = 0
-AND deadline = CURDATE() GROUP BY u.id";
+AND deadline = CURDATE() GROUP BY u.id';
 $result = mysqli_query($connect, $sql);
 $users_current_date = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
@@ -18,20 +18,19 @@ if (!empty($users_current_date)) {
     $mailer = new Swift_Mailer($transport); /* объект библиотеки SwiftMailer, ответственный за отправку сообщений*/
     /* формирует само письмо*/
     foreach ($users_current_date as $key => $value) {
-        $msg = "";
-        $msg_content = "";
+        $msg = '';
+        $msg_content = '';
         /* получает задачи для определенного пользователя*/
-        $sql_tasks = "SELECT * FROM task WHERE status = 0 AND deadline = CURDATE() AND author_id = " . $value['id'];
+        $sql_tasks = 'SELECT * FROM task WHERE status = 0 AND deadline = CURDATE() AND author_id = ' . $value['id'];
         $result = mysqli_query($connect, $sql_tasks);
         $user_tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
         /* меняет имя для каждого пользователя*/
-        $msg_content = "Уважаемый(-ая) " . $value['name'] . " у вас запланирована задача(-и) ";
+        $msg_content = 'Уважаемый(-ая) ' . $value['name'] . ' у вас запланирована задача(-и) ';
         /* обнуляет переменную для работы*/
-        $msg = "";
         /* формирует список задач для пользователя*/
         foreach ($user_tasks as $k => $task) {
-            $msg .= "<br>" . $task['name'] . " на дату: " . correct_visual_date($task['deadline']) . " ";
+            $msg .= '<br>' . $task['name'] . ' на дату: ' . correct_visual_date($task['deadline']) . '';
         }
         /* формирует шаблон письма*/
         $message = new Swift_Message('Уведомление от сервиса «Дела в порядке»');
